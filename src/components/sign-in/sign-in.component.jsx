98f1a09a6ac4358a9def3259ff6bccb525504314
Chaitu <chaitu.ca539@gmail.com>
2020-10-08 17:22:29 +0530

    Add Google Authentication

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 export class SignIn extends Component {
   constructor(props) {
@@ -45,6 +46,9 @@ export class SignIn extends Component {
             required
           />
           <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sign With Google
+          </CustomButton>
         </form>
       </div>
     );
