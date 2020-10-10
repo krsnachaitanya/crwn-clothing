befa3b13a5916e0311cad824b7d33b51518b813a
Chaitu <chaitu.ca539@gmail.com>
2020-10-10 19:14:18 +0530

    create redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import store from "./redux/store";
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
