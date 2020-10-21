e17d97a750dde6c41455f85bd6d6f4c3c0c3e2c6
Chaitu <chaitu.ca539@gmail.com>
2020-10-21 21:24:14 +0530

    implement styled component for custom button component

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import "./custom-button.styles.scss";
+import styled from "styled-components";
 
-function CustomButton({
-  children,
-  isGoogleSignIn,
-  providerLogo,
-  ...otherProps
-}) {
-  return (
-    <button
-      className={`${isGoogleSignIn ? "google__signin" : ""} custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-}
+const CustomButton = styled.button(
+  ({ isGoogleSignIn }) => `
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  background-color: ${isGoogleSignIn ? "#4285f4" : "black"};
+  color: white;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  border: 1px solid rgba(0, 0, 0, 0);
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${isGoogleSignIn ? "#357ae8" : "white"};
+    color: ${isGoogleSignIn ? "white" : "black"};
+    border: 1px solid;
+    border-color: ${isGoogleSignIn ? "#4285f4" : "black"};
+  }
+`
+);
 
 export default CustomButton;
