50a93ddcaac22cffc054138ddb32d3ba02bd4ded
Chaitu <chaitu.ca539@gmail.com>
2020-10-31 23:19:57 +0530

    Move directory and shop items into redux store

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./collections-overview.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+import { selectCollections } from "../../redux/shop/shop.selector.js";
+
+function CollectionsOverview({ collections }) {
+  return (
+    <div className="collections__overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+}
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,12 @@
-import React, { Component } from "react";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
-import SHOP_DATA from "./shop.data.js";
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-export class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
+function ShopPage({ collections }) {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
 }
 
 export default ShopPage;
