fafeb131c72183cc76c7f7bc8bc13c959a7e07a8
Chaitu <chaitu.ca539@gmail.com>
2020-11-01 21:49:17 +0530

    update

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 <h1 align="center">Welcome to crwn-clothing 👋</h1>
-<p>
-</p>
 
-> Clothing ECommerce website built with React.js and Stripe payment integration
+## Clothing ECommerce website built with React.js and Stripe payment integration
 
-## Author
+### Author
 
 👤 **Krishna Chaitanya**
 
 - Github: [@krsnachaitanya](https://github.com/krsnachaitanya)
-
-## Show your support
-
-Give a ⭐️ if this project helped you!
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyC5Z_fT6m3HILqWFUV8pnl-uQd2uYFDx1M",
+  authDomain: "cwrn-clothing.firebaseapp.com",
+  databaseURL: "https://cwrn-clothing.firebaseio.com",
+  projectId: "cwrn-clothing",
+  storageBucket: "cwrn-clothing.appspot.com",
+  messagingSenderId: "570495451571",
+  appId: "1:570495451571:web:6fb371c528270ef9268bc6",
+  measurementId: "G-EXK06MSSP6",
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = db.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error! Creating User", error.message);
+    }
+  }
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const db = firebaseApp.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ promt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
